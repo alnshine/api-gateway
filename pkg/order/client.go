cd9969cba47e3d2f3ef877a2e841c95cace2526a
alnur <abdykalykovalnur@gmail.com>
2023-11-02 15:46:02 +0600

    add proto files, register, login,Authentication,create_order

---
+++ pkg/order/client.go
@@ -0,0 +1,21 @@
+package order
+
+import (
+	"fmt"
+
+	"github.com/alnshine/go-grpc-api-gateway/pkg/config"
+	"github.com/alnshine/go-grpc-api-gateway/pkg/order/pb"
+	"google.golang.org/grpc"
+)
+
+type ServiceClient struct {
+	Client pb.OrderServiceClient
+}
+
+func InitServiceClient(c *config.Config) pb.OrderServiceClient {
+	cc, err := grpc.Dial(c.OrderSvcUrl, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("Could not connect:", err)
+	}
+	return pb.NewOrderServiceClient(cc)
+}
